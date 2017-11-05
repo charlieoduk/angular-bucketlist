6eb863a9a8e4a7fc7c60eac596381016cf5b486a
charlieoduk <odukjr@gmail.com>
2017-11-05 20:13:12 +0300

    feat(authentication): add authentication
      Currently the application has no functionality other than
      the default start page.
    
      This commit adds authentication functionality by adding components
      and services.
    
      [Finishes #152480066]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdButtonModule, MdCheckboxModule, MdGridListModule, MdInputModule, MdIconModule } from '@angular/material';
+import { appRouterModule } from './app.routes';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdGridListModule,
+    MdInputModule,
+    MdIconModule,
+    BrowserAnimationsModule,
+    appRouterModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    AuthenticationService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
